efe7a79b2d451d45478c31997dd062fc0ee440be
mahjabin2024 <mahjabin.mahjabin@opetus.tampere.fi>
2025-07-15 14:11:24 +0300

    flask bmi

---
+++ app.py
@@ -0,0 +1,35 @@
+from flask import Flask, render_template, request
+
+app=Flask(__name__)
+
+
+
+@app.route('/',methods=['GET','POST'])
+
+def index():
+    bmi=None
+    category=""
+    if request.method =='POST':
+        try:
+            weight=float(request.form['weight'])
+            height=float(request.form['height'])
+            if height>0:
+               height=height/100
+               bmi=round((weight/height**2),2)
+            if bmi<18.5:
+               category='underweight'
+            elif 18.5<= bmi<24.9:
+               category='normal weight'
+            elif 25<= bmi <29.9:
+               category='overweight'
+            else:
+               category='obese'
+        except(ValueError, ZeroDivisionError):
+           bmi="Invalid Input"
+           category="Please enter valid weight and height"
+    return render_template('bmi.html',bmi=bmi,category=category)
+if __name__=='__main__':
+   app.run(debug=True)
+  
+
+          
